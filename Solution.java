dfe09821e857810dd39a76b46ac767a2e3637eae
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 23:15:58 -0700

    brute force without stringbuilder usage

--- Solution.java
+++ Solution.java
@@ -1,16 +1,35 @@
+import java.util.*;
+
 public class Solution {
     public String addStrings(String num1, String num2) {
-        System.out.println(stringToValue(num1));
-        System.out.println(stringToValue(num2));
-        return "";//Long.toString(stringToValue(num1) + stringToValue(num2));
-    }
+        List<Character> answer = new ArrayList<>();
+        int num1Index = num1.length() - 1;
+        int num2Index = num2.length() - 1;
+        int carry = 0;
+        while (num1Index >= 0 || num2Index >= 0) {
+            int sum = carry;
 
-    public long stringToValue(String num) {
-        long value = 0;
-        for (int index = num.length() - 1; index >= 0; index--) {
-            int digitVal = Character.getNumericValue(num.charAt(index));
-            value += digitVal * Math.pow(10, num.length() - (index + 1));
+            if (num1Index >= 0)
+                sum += Character.getNumericValue(num1.charAt(num1Index--));
+
+            if (num2Index >= 0)
+                sum += Character.getNumericValue(num2.charAt(num2Index--));
+
+            carry = sum > 9 ? 1 : 0;
+            answer.add(Character.forDigit(sum % 10, 10));
         }
-        return value;
+        if (carry != 0)
+            answer.add(Character.forDigit(carry, 10));
+
+        String ans = "";
+        for (int index = 0; index < answer.size() / 2; index++) {
+            Character valAt = answer.get(index);
+            answer.set(index, answer.get((answer.size() - 1) - index));
+            answer.set((answer.size() - 1) - index, valAt);
+        }
+        for (char charval : answer) {
+            ans += charval;
+        }
+        return ans;
     }
 }
