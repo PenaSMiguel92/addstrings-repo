c7a96c7286a2413de9e9bcaaba47b5f433c99194
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 22:06:26 -0700

    second attempt, still can't have it read the right values, need to perform some magic.

--- Solution.java
+++ Solution.java
@@ -1,5 +1,16 @@
 public class Solution {
     public String addStrings(String num1, String num2) {
-        return "";
+        System.out.println(stringToValue(num1));
+        System.out.println(stringToValue(num2));
+        return "";//Long.toString(stringToValue(num1) + stringToValue(num2));
+    }
+
+    public long stringToValue(String num) {
+        long value = 0;
+        for (int index = num.length() - 1; index >= 0; index--) {
+            int digitVal = Character.getNumericValue(num.charAt(index));
+            value += digitVal * Math.pow(10, num.length() - (index + 1));
+        }
+        return value;
     }
 }
